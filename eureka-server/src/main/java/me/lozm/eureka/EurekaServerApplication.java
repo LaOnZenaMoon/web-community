4e5e6a655d4188d7376233843926e13b218f8284
junlee <laonzenamoon@gmail.com>
2020-11-25 15:27:24 +0900

    feat: update the module "eureka-server"
    add the sources and config files

---
+++ eureka-server/src/main/java/me/lozm/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package me.lozm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplicationBuilder()
+                .sources(EurekaServerApplication.class)
+                .listeners(new ApplicationPidFileWriter("./application.pid"))
+                .build();
+        application.run(args);
+    }
+}
---
+++ eureka-server/src/main/java/me/lozm/eureka/global/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package me.lozm.eureka.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"!test"})
+@Configuration
+public class ApplicationConfig {
+//    @Bean
+//    public GracefulShutdown gracefulShutdown() {
+//        return new GracefulShutdown();
+//    }
+//
+//    @Bean
+//    public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdown gracefulShutdown) {
+//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//        factory.addConnectorCustomizers(gracefulShutdown);
+//        return factory;
+//    }
+}
