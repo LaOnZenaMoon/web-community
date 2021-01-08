45bd1eb6e884bb83c102d6d8c85af2eba3a6f065
junlee <laonzenamoon@gmail.com>
2021-01-08 17:35:19 +0900

    feat($compile): replace how to call HTTP through AXIOS

--- back-office-web/src/main/resources/static/libs/inspinia/js/lozm.js
+++ back-office-web/src/main/resources/static/libs/inspinia/js/lozm.js
@@ -1,25 +1,25 @@
 (function (global, factory) {
-    "use strict";
+    'use strict';
 
-    if (typeof module === "object" && typeof module.exports === "object") {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
         module.exports = global.document ? factory(global, true) : function (w) {
             if (!w.document) {
-                throw new Error("lozm DataTable script Javascript requires a window with a document.");
+                throw new Error('lozm DataTable script Javascript requires a window with a document.');
             }
             return factory(w);
         }
     } else {
         factory(global);
     }
-})(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
-    "use strict";
+})(typeof window !== 'undefined' ? window : this, function (window, noGlobal) {
+    'use strict';
 
     var document = window.document;
-    var version = "0.0.1";
+    var version = '0.0.1';
     var lozm = function (selector) {
         return new lozm.func.init(selector);
     };
-    var JWT_TOKEN = "jwtToken";
+    var JWT_TOKEN = 'jwtToken';
 
     lozm.func = lozm.prototype = {
         version: version
@@ -43,13 +43,13 @@
 
     //Gateway Address
     //Local
-    lozm.GATEWAY_SERVER = "http://127.0.0.1:8882";
+    lozm.GATEWAY_SERVER = 'http://127.0.0.1:8882';
 
     //Prod
-    // lozm.GATEWAY_SERVER = "http://10.110.60.12:8882";
+    // lozm.GATEWAY_SERVER = 'http://10.110.60.12:8882';
 
     var isEmpty = lozm.func.isEmpty = function isEmpty(value) {
-        return value === "" || value === null || value === undefined || typeof value === "undefined" || (typeof value === "object" && !Object.keys(value).length) || (value != null && typeof value === "object" && value.length === 0);
+        return value === '' || value === null || value === undefined || typeof value === 'undefined' || (typeof value === 'object' && !Object.keys(value).length) || (value != null && typeof value === 'object' && value.length === 0);
     };
 
     var isNotEmpty = lozm.func.isNotEmpty = function isNotEmpty(value) {
@@ -59,7 +59,7 @@
     var movePage = lozm.func.movePage = function (_url) {
         if (isEmpty(_url) || checkJwtIsNotValid()) {
             deleteJwt();
-            location.href = "/pages/sign/in";
+            location.href = '/pages/sign/in';
             return;
         }
 
@@ -67,22 +67,22 @@
     };
 
     var showLoadingBar = lozm.func.showLoadingBar = function () {
-        $("#pageloader-overlay").show();
+        $('#pageloader-overlay').show();
     };
 
     var hideLoadingBar = lozm.func.hideLoadingBar = function () {
-        $("#pageloader-overlay").hide();
+        $('#pageloader-overlay').hide();
     };
 
     var receiveHttpError = lozm.func.receiveHttpError = function (xhr, status, error) {
-        alertFail(error);
+        alertFail('Error has occurred. If this situation continues, please contact the administrator.');
         console.log(xhr);
         console.log(status);
         console.log(error);
     }
 
     var setHttpDefault = function (options) {
-        if(options !== undefined && !options.url.includes("/api/sign/in")) {
+        if(options !== undefined && !options.url.includes('/api/sign/in')) {
             if(checkJwtIsNotValid()) movePage();
         }
 
@@ -122,68 +122,28 @@
             .then(hideLoadingBar);
     };
 
-    lozm.func.setDetail = function (_id, _data) {
-        if (isNotEmpty(_data)) {
-            for (var _idx in _data) {
-                $("#" + _id + _idx).val(_data[_idx]);
-            }
-        }
-    };
-
-    lozm.func.changeFileLabel = function (_targetId) {
-        $("#" + _targetId).change(function () {
-            $("#" + _targetId).next(".custom-file-label").text(this.files[0].name);
-        });
-    };
-
-    lozm.func.checkFileType = function (_targetId, _fileType) {
-        var _fileName = $("#" + _targetId).next(".custom-file-label").text();
-        if (_fileName.toLowerCase().indexOf(_fileType.toLowerCase()) == -1) return true;
-        return false;
-    };
-
-    lozm.func.requestMultipartFormData = function (options) {
-        setRequestMultipartFormData(options);
-    };
-
-    var setRequestMultipartFormData = function (options) {
-        var ajaxOptions = $.extend({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "",
-            processData: false,
-            contentType: false,
-            cache: false,
-            timeout: 600000,
-        }, options);
-
-        ajaxOptions.data = options.data;
-
-        requestAjax(ajaxOptions);
-    };
-
     lozm.func.alertSuccess = function (_contents) {
-        swal("Success", isEmpty(_contents) ? "" : _contents, "success");
+        swal('Success', isEmpty(_contents) ? '' : _contents, 'success');
     };
 
     var alertFail = lozm.func.alertFail = function (_contents) {
-        swal("Error", isEmpty(_contents) ? "" : _contents, "error");
+        swal('Error', isEmpty(_contents) ? '' : _contents, 'error');
     };
 
     lozm.func.alertWarning = function (_contents) {
-        swal("Warning", isEmpty(_contents) ? "" : _contents, "warning");
+        swal('Warning', isEmpty(_contents) ? '' : _contents, 'warning');
     };
 
     lozm.func.alertRowIsSelected = function () {
-        swal("Error", "At least select one row.", "error");
+        swal('Error', 'At least select one row.', 'error');
     };
 
     lozm.func.alertRowsAreSelected = function () {
-        swal("Error", "Cannot select more then one row.", "error");
+        swal('Error', 'Cannot select more then one row.', 'error');
     };
 
     lozm.func.datetimepicker = function (_id) {
-        $("#" + _id).datetimepicker({
+        $('#' + _id).datetimepicker({
             format: 'yyyy-mm-ddThh:ii:ss',
         });
     }
@@ -208,7 +168,7 @@
 
     var getJwt = lozm.func.getJwt = function () {
         var _jwtToken = window.localStorage.getItem(JWT_TOKEN);
-        return _jwtToken == null ? null : "Bearer " + _jwtToken;
+        return _jwtToken == null ? null : 'Bearer ' + _jwtToken;
     }
 
     lozm.func.getUserInfoFromJwt = function () {
@@ -241,7 +201,7 @@
 
     lozm.func.signOut = function () {
         deleteJwt();
-        location.href = "/pages/sign/in";
+        location.href = '/pages/sign/in';
     }
 
     init.prototype = lozm.func;
