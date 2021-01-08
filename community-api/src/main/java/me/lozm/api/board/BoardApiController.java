d962c1b8fb2cb2157f64ef67f9433864effc3b51
junlee <laonzenamoon@gmail.com>
2021-01-08 17:42:42 +0900

    feat($compile): modify the response DTO class

--- community-api/src/main/java/me/lozm/api/board/BoardApiController.java
+++ community-api/src/main/java/me/lozm/api/board/BoardApiController.java
@@ -2,8 +2,6 @@ package me.lozm.api.board;
 
 import lombok.RequiredArgsConstructor;
 import me.lozm.object.code.BoardType;
-import me.lozm.object.dto.ApiResponseCode;
-import me.lozm.object.dto.ApiResponseDto;
 import me.lozm.object.dto.board.*;
 import me.lozm.object.vo.board.BoardVo;
 import me.lozm.object.vo.board.CommentVo;
@@ -21,68 +19,58 @@ public class BoardApiController {
 
 
     @GetMapping("/boardType/{boardType}")
-    public ApiResponseDto getBoardList(@PathVariable(value = "boardType") BoardType boardType, Pageable pageable) {
+    public BoardGetDto.Response getBoardList(@PathVariable(value = "boardType") BoardType boardType, Pageable pageable) {
         BoardGetDto.Response resDto = new BoardGetDto.Response();
         resDto.setList(boardService.getBoardList(boardType, pageable));
 
-        return ApiResponseDto.createException(ApiResponseCode.OK, resDto);
+        return resDto;
     }
 
     @GetMapping("/{boardId}")
-    public ApiResponseDto getBoardDetail(@PathVariable(value = "boardId") Long boardId) {
-        return ApiResponseDto.createException(ApiResponseCode.OK, BoardGetDto.of(boardService.getBoardDetail(boardId)));
+    public BoardGetDto getBoardDetail(@PathVariable(value = "boardId") Long boardId) {
+        return BoardGetDto.of(boardService.getBoardDetail(boardId));
     }
 
     @PostMapping
-    public ApiResponseDto postBoard(@RequestBody @Valid BoardPostDto.Request reqDto) {
+    public void postBoard(@RequestBody @Valid BoardPostDto.Request reqDto) {
         boardService.save(BoardVo.of(reqDto));
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @PutMapping
-    public ApiResponseDto putBoard(@RequestBody @Valid BoardPutDto.Request reqDto) {
+    public void putBoard(@RequestBody @Valid BoardPutDto.Request reqDto) {
         boardService.update(BoardVo.of(reqDto));
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @DeleteMapping
-    public ApiResponseDto deleteBoard(@RequestBody @Valid BoardDeleteDto.Request reqDto) {
+    public void deleteBoard(@RequestBody @Valid BoardDeleteDto.Request reqDto) {
         for(BoardDeleteDto dto : reqDto.getList()) {
             boardService.delete(BoardVo.of(reqDto.getModifiedBy(), dto));
         }
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @GetMapping("/{boardId}/comment")
-    public ApiResponseDto getCommentList(@PathVariable(value = "boardId") Long boardId, Pageable pageable) {
+    public CommentGetDto.Response getCommentList(@PathVariable(value = "boardId") Long boardId, Pageable pageable) {
         CommentGetDto.Response resDto = new CommentGetDto.Response();
         resDto.setList(boardService.getCommentList(boardId, pageable));
 
-        return ApiResponseDto.createException(ApiResponseCode.OK, resDto);
+        return resDto;
     }
 
     @PostMapping("/comment")
-    public ApiResponseDto postComment(@RequestBody @Valid CommentPostDto.Request reqDto) {
+    public void postComment(@RequestBody @Valid CommentPostDto.Request reqDto) {
         boardService.save(CommentVo.of(reqDto));
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @PutMapping("/comment")
-    public ApiResponseDto putComment(@RequestBody @Valid CommentPutDto.Request reqDto) {
+    public void putComment(@RequestBody @Valid CommentPutDto.Request reqDto) {
         boardService.update(CommentVo.of(reqDto));
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @DeleteMapping("/comment")
-    public ApiResponseDto deleteComment(@RequestBody @Valid CommentDeleteDto.Request reqDto) {
+    public void deleteComment(@RequestBody @Valid CommentDeleteDto.Request reqDto) {
         for(CommentDeleteDto dto : reqDto.getList()) {
             boardService.delete(CommentVo.of(reqDto.getModifiedBy(), dto));
         }
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
 }
--- community-api/src/main/java/me/lozm/api/user/UserApiController.java
+++ community-api/src/main/java/me/lozm/api/user/UserApiController.java
@@ -1,10 +1,7 @@
 package me.lozm.api.user;
 
 import lombok.RequiredArgsConstructor;
-import me.lozm.object.code.BoardType;
 import me.lozm.object.code.UsersType;
-import me.lozm.object.dto.ApiResponseCode;
-import me.lozm.object.dto.ApiResponseDto;
 import me.lozm.object.dto.user.UserDeleteDto;
 import me.lozm.object.dto.user.UserGetDto;
 import me.lozm.object.dto.user.UserPostDto;
@@ -16,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
-
 @CrossOrigin("*")
 @RestController @RequestMapping(value = "/api/user")
 @RequiredArgsConstructor
@@ -28,34 +23,28 @@ public class UserApiController {
 
 
     @GetMapping("/usersType/{usersType}")
-    public ApiResponseDto getUserList(@PathVariable(value = "usersType") UsersType usersType, Pageable pageable) {
+    public UserGetDto.Response getUserList(@PathVariable(value = "usersType") UsersType usersType, Pageable pageable) {
         UserGetDto.Response resDto = new UserGetDto.Response();
         resDto.setList(userService.getUserList(usersType, pageable));
 
-        return ApiResponseDto.createException(ApiResponseCode.OK, resDto);
+        return resDto;
     }
 
     @PostMapping
-    public ApiResponseDto postUser(@RequestBody @Valid UserPostDto.Request reqDto) {
+    public void postUser(@RequestBody @Valid UserPostDto.Request reqDto) {
          userService.save(UserVo.of(reqDto, passwordEncoder.encode(reqDto.getPassword())));
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @PutMapping
-    public ApiResponseDto putUser(@RequestBody @Valid UserPutDto.Request reqDto) {
+    public void putUser(@RequestBody @Valid UserPutDto.Request reqDto) {
         userService.update(UserVo.of(reqDto, passwordEncoder.encode(reqDto.getPassword())));
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
     @DeleteMapping
-    public ApiResponseDto deleteUser(@RequestBody @Valid UserDeleteDto.Request reqDto) {
+    public void deleteUser(@RequestBody @Valid UserDeleteDto.Request reqDto) {
         for(UserDeleteDto dto : reqDto.getList()) {
             userService.delete(UserVo.of(reqDto.getModifiedBy(), dto));
         }
-
-        return ApiResponseDto.createException(ApiResponseCode.OK, null);
     }
 
 }
