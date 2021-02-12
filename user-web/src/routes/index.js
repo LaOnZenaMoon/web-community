fccbaa00c5635884eeabe5d280305004ee739325
junlee <laonzenamoon@gmail.com>
2021-02-12 18:15:15 +0900

    feat: initial commit

---
+++ user-web/src/routes/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Main from '../pages/Main';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'Main',
+        component: Main,
+    },
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
+export default router;
--- user-web/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-// Vuex 를 전역적으로 사용하기 위함
-Vue.use(Vuex);
-
-export const store = new Vuex.store({
-
-});
