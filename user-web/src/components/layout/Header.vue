cd928d9d52cccc427103ab0317db1a608f1e7ab0
junlee <laonzenamoon@gmail.com>
2021-02-14 22:04:53 +0900

    feat: modify header

--- user-web/src/components/layout/Header.vue
+++ user-web/src/components/layout/Header.vue
@@ -1,41 +1,30 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark" class="menu">
-      <b-navbar-brand href="/">LaOnZenaMoon</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse is-nav>
-        <b-navbar-nav>
-          <b-nav-item>Notice</b-nav-item>
-          <b-nav-item>Column</b-nav-item>
-          <b-nav-item-dropdown
-            text="Community"
-            toggle-class="nav-link-custom"
-            right
-          >
-            <b-dropdown-item>Expert community</b-dropdown-item>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item>Free community</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item>Multimedia</b-nav-item>
-          <b-nav-item>Comedy</b-nav-item>
-          <b-nav-item>Market</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <b-nav class="menu1">
+      <b-nav-item-dropdown right>
+        <template #button-content>
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-nav>
+    <b-nav class="menu2">
+      <b-nav-item>Notice</b-nav-item>
+      <b-nav-item>Column</b-nav-item>
+      <b-nav-item-dropdown
+        text="Community"
+        toggle-class="nav-link-custom"
+        right
+      >
+        <b-dropdown-item>Expert community</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item>Free community</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item>Multimedia</b-nav-item>
+      <b-nav-item>Comedy</b-nav-item>
+      <b-nav-item>Market</b-nav-item>
+    </b-nav>
   </div>
 </template>
 
@@ -44,7 +33,10 @@ export default {}
 </script>
 
 <style scoped>
-.menu {
+.menu1 {
+  background-color: lightgrey;
+}
+.menu2 {
   background-color: #CC9999;
 }
 </style>
