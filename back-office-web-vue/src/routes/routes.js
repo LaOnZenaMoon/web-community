a66a68ede24fbabe373a930d7ed00c468de49c44
junlee <laonzenamoon@gmail.com>
2021-02-19 16:54:53 +0900

    feat: developing the main page layout

--- back-office-web-vue/src/routes/routes.js
+++ back-office-web-vue/src/routes/routes.js
@@ -1,7 +1,9 @@
 import SignIn from "@/views/lozm/SignIn";
 import MainLayout from "@/views/lozm/MainLayout";
-import NotFoundPage from "@/views/NotFoundPage";
 import Main from "@/views/lozm/Main";
+import NotFound from "@/views/lozm/NotFound";
+import DashboardLayout from "@/views/Layout/DashboardLayout";
+import AuthLayout from "@/views/Pages/AuthLayout";
 
 const routes = [
   {
@@ -9,71 +11,72 @@ const routes = [
     component: SignIn,
   },
   {
-    path: '/',
-    redirect: 'main',
+    path: '/page',
     component: MainLayout,
     children: [
       {
         path: '/main',
+        name: 'main',
         component: Main
       },
       {
         path: '*',
-        component: NotFoundPage
+        name: 'Not Found',
+        component: NotFound
       }
     ],
   },
 
-  // {
-  //   path: '/',
-  //   redirect: 'dashboard',
-  //   component: DashboardLayout,
-  //   children: [
-  //     {
-  //       path: '/dashboard',
-  //       name: 'dashboard',
-  //       component: () => import('../views/Dashboard.vue')
-  //     },
-  //     {
-  //       path: '/icons',
-  //       name: 'icons',
-  //       component: () => import('../views/Icons.vue')
-  //     },
-  //     {
-  //       path: '/profile',
-  //       name: 'profile',
-  //       component: () => import('../views/Pages/UserProfile.vue')
-  //     },
-  //     {
-  //       path: '/maps',
-  //       name: 'maps',
-  //       component: () => import('../views/GoogleMaps.vue')
-  //     },
-  //     {
-  //       path: '/tables',
-  //       name: 'tables',
-  //       component: () => import('../views/RegularTables.vue')
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/',
-  //   redirect: 'login',
-  //   component: AuthLayout,
-  //   children: [
-  //     {
-  //       path: '/login',
-  //       name: 'login',
-  //       component: () => import('../views/Pages/Login.vue')
-  //     },
-  //     {
-  //       path: '/register',
-  //       name: 'register',
-  //       component: () => import('../views/Pages/Register.vue')
-  //     },
-  //     { path: '*', component: NotFound }
-  //   ]
-  // }
+  {
+    path: '/test',
+    redirect: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('../views/Dashboard.vue')
+      },
+      {
+        path: '/icons',
+        name: 'icons',
+        component: () => import('../views/Icons.vue')
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/Pages/UserProfile.vue')
+      },
+      {
+        path: '/maps',
+        name: 'maps',
+        component: () => import('../views/GoogleMaps.vue')
+      },
+      {
+        path: '/tables',
+        name: 'tables',
+        component: () => import('../views/RegularTables.vue')
+      }
+    ]
+  },
+  {
+    path: '/test',
+    redirect: 'login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/Pages/Login.vue')
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: () => import('../views/Pages/Register.vue')
+      },
+      { path: '*', component: NotFound }
+    ]
+  }
 ];
 
 export default routes;
--- back-office-web-vue/src/views/lozm/LeftSideBar.vue
+++ back-office-web-vue/src/views/lozm/LeftSideBar.vue
@@ -1,92 +1,129 @@
 <template>
-  <side-bar>
-    <template slot="links">
-      <sidebar-item
-        :link="{
-            name: 'Dashboard',
-            path: '/dashboard',
-            icon: 'ni ni-tv-2 text-primary',
-          }"
-      >
-      </sidebar-item>
+  <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+    <div class="container-fluid">
 
-      <sidebar-item
-        :link="{
-              name: 'Icons',
-              path: '/icons',
-              icon: 'ni ni-planet text-blue'
-              }"
-      >
-      </sidebar-item>
+      <!--Toggler-->
+      <navbar-toggle-button @click.native="showSidebar">
 
-      <sidebar-item
-        :link="{
-                name: 'Maps',
-                path: '/maps',
-                icon: 'ni ni-pin-3 text-orange'
-              }">
-      </sidebar-item>
+      </navbar-toggle-button>
+      <router-link class="navbar-brand" to="/">
+<!--        <img :src="logo" class="navbar-brand-img" alt="...">-->
+        <h3>LaOnZenaMoon</h3>
+      </router-link>
 
-      <sidebar-item
-        :link="{
-                name: 'User Profile',
-                path: '/profile',
-                icon: 'ni ni-single-02 text-yellow'
-                }">
-      </sidebar-item>
+      <slot name="mobile-right">
+        <ul class="nav align-items-center d-md-none">
+          <base-dropdown class="nav-item" menu-on-right tag="li" title-tag="a">
+            <a slot="title-container" class="nav-link nav-link-icon" href="#" role="button"
+               aria-haspopup="true" aria-expanded="false">
+              <i class="ni ni-bell-55"></i>
+            </a>
 
-      <sidebar-item
-        :link="{
-                  name: 'Tables',
-                  path: '/tables',
-                  icon: 'ni ni-bullet-list-67 text-red'
-                }">
-      </sidebar-item>
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </base-dropdown>
+          <base-dropdown class="nav-item" menu-on-right tag="li" title-tag="a">
+            <a slot="title-container" class="nav-link" href="#" role="button">
+              <div class="media align-items-center">
+                              <span class="avatar avatar-sm rounded-circle">
+                                <img alt="Image placeholder" src="img/theme/team-1.jpg">
+                              </span>
+              </div>
+            </a>
 
-      <sidebar-item
-        :link="{
-                    name: 'Login',
-                    path: '/login',
-                    icon: 'ni ni-key-25 text-info'
-                  }">
-      </sidebar-item>
-      <sidebar-item
-        :link="{
-                    name: 'Register',
-                    path: '/register',
-                    icon: 'ni ni-circle-08 text-pink'
-                  }">
-      </sidebar-item>
-    </template>
+            <div class=" dropdown-header noti-title">
+              <h6 class="text-overflow m-0">Welcome!</h6>
+            </div>
+            <router-link to="/profile" class="dropdown-item">
+              <i class="ni ni-single-02"></i>
+              <span>My profile</span>
+            </router-link>
+            <router-link to="/profile" class="dropdown-item">
+              <i class="ni ni-settings-gear-65"></i>
+              <span>Settings</span>
+            </router-link>
+            <router-link to="/profile" class="dropdown-item">
+              <i class="ni ni-calendar-grid-58"></i>
+              <span>Activity</span>
+            </router-link>
+            <router-link to="/profile" class="dropdown-item">
+              <i class="ni ni-support-16"></i>
+              <span>Support</span>
+            </router-link>
+            <div class="dropdown-divider"></div>
+            <a href="#!" class="dropdown-item">
+              <i class="ni ni-user-run"></i>
+              <span>Logout</span>
+            </a>
+          </base-dropdown>
+        </ul>
+      </slot>
+      <slot></slot>
+      <div v-show="$sidebar.showSidebar" class="navbar-collapse collapse show" id="sidenav-collapse-main">
 
-    <template slot="links-after">
-      <hr class="my-3">
-      <h6 class="navbar-heading p-0 text-muted">Documentation</h6>
+        <div class="navbar-collapse-header d-md-none">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <router-link to="/">
+                <img :src="logo">
+              </router-link>
+            </div>
+            <div class="col-6 collapse-close">
+              <navbar-toggle-button @click.native="closeSidebar"></navbar-toggle-button>
+            </div>
+          </div>
+        </div>
 
-      <b-nav class="navbar-nav mb-md-3">
-        <b-nav-item
-          href="https://www.creative-tim.com/learning-lab/bootstrap-vue/quick-start/argon-dashboard"
-        >
-          <i class="ni ni-spaceship"></i>
-          <b-nav-text class="p-0">Getting started</b-nav-text>
-        </b-nav-item>
-        <b-nav-item href="https://www.creative-tim.com/learning-lab/bootstrap-vue/colors/argon-dashboard">
-          <i class="ni ni-palette"></i>
-          <b-nav-text class="p-0">Foundation</b-nav-text>
-        </b-nav-item>
-        <b-nav-item href="https://www.creative-tim.com/learning-lab/bootstrap-vue/avatar/argon-dashboard">
-          <i class="ni ni-ui-04"></i>
-          <b-nav-text class="p-0">Components</b-nav-text>
-        </b-nav-item>
-      </b-nav>
-    </template>
-  </side-bar>
+        <ul class="navbar-nav">
+          <slot name="links">
+          </slot>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
+import NavbarToggleButton from "@/components/NavbarToggleButton";
+
 export default {
-  name: "LeftSideBar"
-}
+  name: 'sidebar',
+  components: {
+    NavbarToggleButton
+  },
+  props: {
+    logo: {
+      type: String,
+      default: 'img/brand/green.png',
+      description: 'Sidebar app logo'
+    },
+    autoClose: {
+      type: Boolean,
+      default: true,
+      description: 'Whether sidebar should autoclose on mobile when clicking an item'
+    }
+  },
+  provide() {
+    return {
+      autoClose: this.autoClose
+    };
+  },
+  methods: {
+    closeSidebar() {
+      this.$sidebar.displaySidebar(false)
+    },
+    showSidebar() {
+      this.$sidebar.displaySidebar(true)
+    }
+  },
+  beforeDestroy() {
+    if (this.$sidebar.showSidebar) {
+      this.$sidebar.showSidebar = false;
+    }
+  }
+};
 </script>
 
 <style scoped>
