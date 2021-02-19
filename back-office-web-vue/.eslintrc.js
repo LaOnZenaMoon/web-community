408b5d7bdf720a653d624b5fc0c3808fcefac847
junlee <laonzenamoon@gmail.com>
2021-02-19 11:15:50 +0900

    feat: initial commit back-office-web-vue
    - back-office-web with Vue.js module

---
+++ back-office-web-vue/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    "browser": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+  }
+};
+
