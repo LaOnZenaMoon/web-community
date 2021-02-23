e84d4b531768381a95967ccddb1bb5cd4f231154
junlee <laonzenamoon@gmail.com>
2021-02-23 09:22:21 +0900

    feat: detach fontAwesomeIcon.js

---
+++ user-web/src/common/fontAwesomeIcon.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons';
+import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+library.add(
+  fasHeart,
+  farHeart
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
--- user-web/src/main.js
+++ user-web/src/main.js
@@ -5,23 +5,11 @@ import store from './store';
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "./common/fontAwesomeIcon.js";
 
-//fort-awesome
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons';
-import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-
-library.add(
-  fasHeart,
-  farHeart
-);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
 Vue.config.productionTip = false;
 
 new Vue({
